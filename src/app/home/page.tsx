324e25f75bce9600497fff8f3a1358e4e053c3bc
baglaev <bglvssh@yandex.ru>
2024-10-20 22:52:53 +0300

    git init and fsd begin

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+import styles from './page.module.scss';
+import Header from '@/widgets/header/ui/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
---
+++ src/widgets/header/ui/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>hi</h1>
+      <div className={styles.leftColumns}></div>
+    </header>
+  );
+}
