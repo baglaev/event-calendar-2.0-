5b4fca8a6d2d5a5fb688982580fc24eb6a79275f
baglaev <bglvssh@yandex.ru>
2024-10-21 23:33:18 +0300

    add: redirect, fonts, edit header

---
+++ src/features/auth/ui/LoginButton.tsx
@@ -0,0 +1,4 @@
+import styles from './styles.module.scss';
+export function LoginButton() {
+  return <button className={styles.loginButton}>Войти</button>;
+}
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,10 +1,28 @@
 import styles from './styles.module.scss';
+import { LoginButton } from '@/features/auth';
 
 export default function Header() {
   return (
-    <header>
-      <h1>hi</h1>
-      <div className={styles.leftColumns}></div>
+    <header className={styles.header}>
+      <div className={styles.header__leftСolumn}>
+        <div className={styles.header__logo}>
+          <img src="" alt="" className={styles.header__logoImg} />
+          red collar
+        </div>
+        <h1 className={styles.header__title}>
+          planner
+          <span className={styles.header__span}> event</span>
+        </h1>
+      </div>
+      <div className={styles.header__rightColumn}>
+        <div className={styles.header__calendarButtons}>
+          <p className={styles.header__calendarMonth}>октябрь</p>
+          <button className={styles.header__calendarButton}></button>
+          <button
+            className={`${styles.header__calendarButton} ${styles.header_buttonNext}`}></button>
+        </div>
+        <LoginButton />
+      </div>
     </header>
   );
 }
